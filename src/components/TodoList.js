ef3b2a5329b075e09263cbdf7b553817553b23c1
Sigmund Burgos <sigmund_burgos@ligph.com>
2021-01-08 14:51:41 +0800

    Use loop

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 import TodoItem from './TodoItem';
 
+const todoItems = [
+  'Wake up',
+  'Eat Breakfast',
+  'Take a Bath',
+  'Go to School',
+  'Learn Something',
+  'Go Home',
+  'Go toSleep'
+];
+
 const TodoList = () => (
   <ul className="todo-list">
-    <li className="todo-list-item">
-      <TodoItem />
-    </li>
-    <li className="todo-list-item">
-      <TodoItem />
-    </li>
-    <li className="todo-list-item">
-      <TodoItem />
-    </li>
-    <li className="todo-list-item">
-      <TodoItem />
-    </li>
-    <li className="todo-list-item">
-      <TodoItem />
-    </li>
+    {todoItems.map(item => (
+      <li key={item} className="todo-list-item">
+        <TodoItem />
+      </li>
+    ))}
   </ul>
 );
 
