e146a2977809abe3d24e888d3672c49b27d95884
Sigmund Burgos <sigmund_burgos@ligph.com>
2021-01-11 13:38:08 +0800

    Add event handler to button and input

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class TodoAdd extends Component {
+  handleAdd() {
+    console.log('add clicked');
+  }
+
+  handleChange(name) {
+    console.log(name);
+  }
+
   render() {
     return (
       <div className="todo-add">
-        <input type="text" className="todo-add-input" />
-        <button className="todo-add-button">Add</button>
+        <input
+          type="text"
+          className="todo-add-input"
+          onChange={e => this.handleChange(e.target.value)}
+        />
+        <button className="todo-add-button" onClick={() => this.handleAdd()}>
+          Add
+        </button>
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component {
+  handleDelete(name) {
+    console.log(`delete ${name}`);
+  }
+
   render() {
     const { todo } = this.props;
 
     return (
       <>
         <p>{todo}</p>
-        <button className="todo-item-delete">Delete</button>
+        <button
+          className="todo-item-delete"
+          onClick={() => this.handleDelete(todo)}
+        >
+          Delete
+        </button>
       </>
     );
   }
