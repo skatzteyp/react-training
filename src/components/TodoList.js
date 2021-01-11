8186a4adcd277eed11ad392a9569861010cead7d
Sigmund Burgos <sigmund_burgos@ligph.com>
2021-01-11 13:27:51 +0800

    Use loop

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 
 import TodoItem from './TodoItem';
 
+const todoItems = [
+  'Wake up',
+  'Eat Breakfast',
+  'Take a Bath',
+  'Go to School',
+  'Learn Something',
+  'Go Home',
+  'Go toSleep'
+];
+
 class TodoList extends Component {
   render() {
     return (
       <ul className="todo-list">
-        <li className="todo-list-item">
-          <TodoItem />
-        </li>
-        <li className="todo-list-item">
-          <TodoItem />
-        </li>
-        <li className="todo-list-item">
-          <TodoItem />
-        </li>
-        <li className="todo-list-item">
-          <TodoItem />
-        </li>
-        <li className="todo-list-item">
-          <TodoItem />
-        </li>
+        {todoItems.map(item => (
+          <li key={item} className="todo-list-item">
+            <TodoItem />
+          </li>
+        ))}
       </ul>
     );
   }
